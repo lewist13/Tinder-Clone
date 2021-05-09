61769b584c60253a290c1be979cd318a7a904e2a
lewist13 <lewistpro@gmail.com>
2021-05-09 17:39:52 -0400

    built example foundation

--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/TinderCards.css";
 
 function TinderCards() {
-  return <div className="tinderCards"></div>;
+  const [people, setPeople] = useState([
+    {
+      name: "Elon Musk",
+      url:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FI%2F41Gr%2BplCXTL.jpg&f=1&nofb=1",
+    },
+  ]);
+
+  return (
+    <div className="tinderCards">
+      {people.map((person) => (
+        <h1>{person.name}</h1>
+      ))}
+    </div>
+  );
 }
 
 export default TinderCards;
