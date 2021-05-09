11b62812eba8be7f51e36be04af672b83e58fded
lewist13 <lewistpro@gmail.com>
2021-05-09 17:41:44 -0400

    rendered tindercards

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import "./styles/App.css";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/** TinderCards */}
+      <TinderCards />
       {/** SwipeButtons */}
     </div>
   );
