01d310460214ecc49e034a77791bbd12c41c7ddd
lewist13 <lewistpro@gmail.com>
2021-05-09 16:43:33 -0400

    updated app file foundations; began creating header

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import "./styles/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>MERN TINDER CLONE</h1>
+
+      {/** Header */}
+      {/** TinderCards */}
+      {/** SwipeButtons */}
+    </div>
+  );
 }
 
 export default App;
