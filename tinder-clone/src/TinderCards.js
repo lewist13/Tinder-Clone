a096443dcf159619a6486d180fe4a2f057e058f0
lewist13 <lewistpro@gmail.com>
2021-05-09 21:01:18 -0400

    updated and completed tinder cards

--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -1,25 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import "./styles/TinderCards.css";
+import axios from "./services/axios";
 
 function TinderCards() {
   // eslint-disable-next-line no-unused-vars
-  const [people, setPeople] = useState([
-    {
-      name: "Elon Musk",
-      url:
-        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.estrategiaynegocios.net%2Fcsp%2Fmediapool%2Fsites%2Fdt.common.streams.StreamServer.cls%3FSTREAMOID%3D8m9GvGM1ggGjC31mHEtEgc%24daE2N3K4ZzOUsqbU5sYv9Y0bpvn4CelVYksY06CmM6FB40xiOfUoExWL3M40tfzssyZqpeG_J0TFo7ZhRaDiHC9oxmioMlYVJD0A%243RbIiibgT65kY_CSDiCiUzvHvODrHApbd6ry6YGl5GGOZrs-%26CONTENTTYPE%3Dimage%2Fjpeg&f=1&nofb=1",
-    },
-    {
-      name: "Jeff Bezos",
-      url:
-        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftalkglitz.media%2Fwp-content%2Fuploads%2F2020%2F07%2FJeff_Bezos_laugh_2.jpg&f=1&nofb=1",
-    },
-  ]);
+  const [people, setPeople] = useState([]);
 
-  const swiped = (direction, nameToDelete) => {
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get("/tinder/cards");
+
+      setPeople(req.data);
+    }
+
+    fetchData();
+  }, []);
+
+  const swiped = (nameToDelete) => {
     console.log(`Removing: ${nameToDelete}`);
-    // setLastDirection(direction);
   };
 
   const outOfFrame = (name) => {
@@ -38,7 +37,7 @@ function TinderCards() {
             onCardLeftScreen={() => outOfFrame(person.name)}
           >
             <div
-              style={{ backgroundImage: `url(${person.url})` }}
+              style={{ backgroundImage: `url(${person.imgUrl})` }}
               className="card"
             >
               <h3>{person.name}</h3>
